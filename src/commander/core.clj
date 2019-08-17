1fb86dc3cf1b8ba6bcf79a102d78d04b43d68840
Daw-Ran Liou <dawran6@gmail.com>
2019-08-16 19:10:47 -0700

    Ring app

--- src/commander/core.clj
+++ src/commander/core.clj
@@ -1,15 +1,30 @@
 (ns commander.core
-  (:require [reitit.ring :as ring]
+  (:require [muuntaja.core :as m]
+            [reitit.ring :as ring]
+            [reitit.ring.middleware.muuntaja :as muuntaja]
             [reitit.swagger :as swagger]
-            [reitit.swagger-ui :as swagger-ui]))
+            [reitit.swagger-ui :as swagger-ui]
+            [ring.adapter.jetty :as jetty]))
 
 (def router
   (ring/router
-   ["/ping" handler]))
+   [["/api"
+     ["/ping"
+      {:get (constantly {:status 200 :body "pong"})}]]
+    ["/swagger.json"
+     {:get {:no-doc  true
+            :handler (swagger/create-swagger-handler)}}]]
+   {:data
+    {:muuntaja   m/instance
+     :middleware [swagger/swagger-feature
+                  muuntaja/format-negotiate-middleware
+                  muuntaja/format-response-middleware
+                  muuntaja/format-request-middleware]}}))
 
 (def default-handler
   (ring/routes
    (ring/redirect-trailing-slash-handler)
+   (swagger-ui/create-swagger-ui-handler {:path "/api-docs"})
    (ring/create-resource-handler)
    (ring/create-default-handler)))
 
@@ -18,12 +33,21 @@
    router
    default-handler))
 
-(app {:request-method :get :uri "/ping/"})
+(defn start
+  []
+  (jetty/run-jetty #'app {:port 3000 :join? false})
+  (println "server running in port 3000"))
 
-(app {:request-method :get :uri "/404"})
+(comment
 
-(app {:request-method :get :uri "/css/hi.css"})
+  (start)
 
-(require '[reitit.core :as r])
+  (app {:request-method :get :uri "/swagger.json"})
 
-(r/match-by-path router "/ping")
+  (app {:request-method :get :uri "/404"})
+
+  (app {:request-method :get :uri "/css/hi.css"})
+
+  (require '[reitit.core :as r])
+
+  (r/match-by-path router "/ping"))
