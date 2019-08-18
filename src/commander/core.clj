0f0d4cc264a018ec2c0bcc529f90ee0a5a9d945f
Daw-Ran Liou <dawran6@gmail.com>
2019-08-17 20:57:53 -0700

    Use reitit example

--- src/commander/core.clj
+++ src/commander/core.clj
@@ -1,53 +1,115 @@
 (ns commander.core
-  (:require [muuntaja.core :as m]
-            [reitit.ring :as ring]
-            [reitit.ring.middleware.muuntaja :as muuntaja]
+  (:require [reitit.ring :as ring]
+            [reitit.coercion.spec]
             [reitit.swagger :as swagger]
             [reitit.swagger-ui :as swagger-ui]
-            [ring.adapter.jetty :as jetty]))
+            [reitit.ring.coercion :as coercion]
+            [reitit.dev.pretty :as pretty]
+            [reitit.ring.middleware.muuntaja :as muuntaja]
+            [reitit.ring.middleware.exception :as exception]
+            [reitit.ring.middleware.multipart :as multipart]
+            [reitit.ring.middleware.parameters :as parameters]
+            [reitit.ring.middleware.dev :as dev]
+            [reitit.ring.spec :as spec]
+            [spec-tools.spell :as spell]
+            [ring.adapter.jetty :as jetty]
+            [muuntaja.core :as m]
+            [clojure.spec.alpha :as s]
+            [clojure.java.io :as io]))
 
-(def router
-  (ring/router
-   [["/api"
-     ["/ping"
-      {:get (constantly {:status 200 :body "pong"})}]]
-    ["/swagger.json"
-     {:get {:no-doc  true
-            :handler (swagger/create-swagger-handler)}}]]
-   {:data
-    {:muuntaja   m/instance
-     :middleware [swagger/swagger-feature
-                  muuntaja/format-negotiate-middleware
-                  muuntaja/format-response-middleware
-                  muuntaja/format-request-middleware]}}))
+(s/def ::file multipart/temp-file-part)
+(s/def ::file-params (s/keys :req-un [::file]))
 
-(def default-handler
-  (ring/routes
-   (ring/redirect-trailing-slash-handler)
-   (swagger-ui/create-swagger-ui-handler {:path "/api-docs"})
-   (ring/create-resource-handler)
-   (ring/create-default-handler)))
+(s/def ::name string?)
+(s/def ::size int?)
+(s/def ::file-response (s/keys :req-un [::name ::size]))
+
+(s/def ::x int?)
+(s/def ::y int?)
+(s/def ::total int?)
+(s/def ::math-request (s/keys :req-un [::x ::y]))
+(s/def ::math-response (s/keys :req-un [::total]))
 
 (def app
   (ring/ring-handler
-   router
-   default-handler))
+    (ring/router
+      [["/swagger.json"
+        {:get {:no-doc true
+               :swagger {:info {:title "my-api"}}
+               :handler (swagger/create-swagger-handler)}}]
 
-(defn start
-  []
-  (jetty/run-jetty #'app {:port 3000 :join? false})
+       ["/files"
+        {:swagger {:tags ["files"]}}
+
+        ["/upload"
+         {:post {:summary "upload a file"
+                 :parameters {:multipart ::file-params}
+                 :responses {200 {:body ::file-response}}
+                 :handler (fn [{{{:keys [file]} :multipart} :parameters}]
+                            {:status 200
+                             :body {:name (:filename file)
+                                    :size (:size file)}})}}]
+
+        ["/download"
+         {:get {:summary "downloads a file"
+                :swagger {:produces ["image/png"]}
+                :handler (fn [_]
+                           {:status 200
+                            :headers {"Content-Type" "image/png"}
+                            :body (io/input-stream
+                                    (io/resource "reitit.png"))})}}]]
+
+       ["/math"
+        {:swagger {:tags ["math"]}}
+
+        ["/plus"
+         {:get {:summary "plus with spec query parameters"
+                :parameters {:query ::math-request}
+                :responses {200 {:body ::math-response}}
+                :handler (fn [{{{:keys [x y]} :query} :parameters}]
+                           {:status 200
+                            :body {:total (+ x y)}})}
+          :post {:summary "plus with spec body parameters"
+                 :parameters {:body ::math-request}
+                 :responses {200 {:body ::math-response}}
+                 :handler (fn [{{{:keys [x y]} :body} :parameters}]
+                            {:status 200
+                             :body {:total (+ x y)}})}}]]]
+
+      {;;:reitit.middleware/transform dev/print-request-diffs ;; pretty diffs
+       ;;:validate spec/validate ;; enable spec validation for route data
+       ;;:reitit.spec/wrap spell/closed ;; strict top-level validation
+       :exception pretty/exception
+       :data {:coercion reitit.coercion.spec/coercion
+              :muuntaja m/instance
+              :middleware [;; swagger feature
+                           swagger/swagger-feature
+                           ;; query-params & form-params
+                           parameters/parameters-middleware
+                           ;; content-negotiation
+                           muuntaja/format-negotiate-middleware
+                           ;; encoding response body
+                           muuntaja/format-response-middleware
+                           ;; exception handling
+                           exception/exception-middleware
+                           ;; decoding request body
+                           muuntaja/format-request-middleware
+                           ;; coercing response bodys
+                           coercion/coerce-response-middleware
+                           ;; coercing request parameters
+                           coercion/coerce-request-middleware
+                           ;; multipart
+                           multipart/multipart-middleware]}})
+    (ring/routes
+      (swagger-ui/create-swagger-ui-handler
+        {:path "/"
+         :config {:validatorUrl nil
+                  :operationsSorter "alpha"}})
+      (ring/create-default-handler))))
+
+(defn start []
+  (jetty/run-jetty #'app {:port 3000, :join? false})
   (println "server running in port 3000"))
 
 (comment
-
-  (start)
-
-  (app {:request-method :get :uri "/swagger.json"})
-
-  (app {:request-method :get :uri "/404"})
-
-  (app {:request-method :get :uri "/css/hi.css"})
-
-  (require '[reitit.core :as r])
-
-  (r/match-by-path router "/ping"))
+  (start))
